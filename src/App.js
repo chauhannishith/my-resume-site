836e0d95f6bbd6651f747c05a651df5736fbe7f5
chauhannishith <chauhannis123ith@gmail.com>
2018-04-26 15:51:40 +0530

    update when free

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Header } from "./components/header";
-/*import logo from './logo.svg';*/
 
 class App extends Component {
   render() {
     return (
       <div className="main">
         <Header/>
-        <h1>Hi, I am Nishith Chauhan</h1>
-        <h3>This website is still under development</h3>
+        <div>
+        	<h1>Hi, I am Nishith Chauhan</h1>
+        	<h3>This website is still under development</h3>
+        </div>
       </div>
     );
   }
